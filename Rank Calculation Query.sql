89a3e68ba9145ae796531b6167a6b59ba4f63105
Raees <mraiskundi@gmail.com>
2023-10-26 20:59:48 +0500

    Update Rank Calculation Query.sql

--- Rank Calculation Query.sql
+++ Rank Calculation Query.sql
@@ -1,3 +1,4 @@
+-- For this query First create a table name (Rank) With column ID,Name,Marks
 SELECT a.ID, a.Name, a.Marks, Count(*) AS Rank
 FROM Rank AS a, RANK AS b
 GROUP BY a.ID, a.Name, a.Marks;
